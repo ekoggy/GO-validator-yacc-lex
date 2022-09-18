d5e18a2c1c9c35a6b4be50495cb816712dd66330
Egor Kondachkov <pon4ik0123@mail.ru>
2022-09-18 16:07:30 +0300

    Update make.ps1

--- make.ps1
+++ make.ps1
@@ -1,8 +1,22 @@
-Remove-item analyser.tab.c
-Remove-item analyser.tab.h
-Remove-item validator.exe
-Remove-item lex.yy.c
-.\..\flex_bison\win_bison.exe -d analyser.y
-.\..\flex_bison\win_flex.exe lexer.l
-g++ lex.yy.c grammar.cpp grammar.h analyser.tab.h analyser.tab.c -o validator
-Get-Content config.in | .\validator.exe
+Param (
+[string]$mode = "all"
+)
+if ($mode -eq "all" -or $mode -eq "clear")
+{
+    echo "Clear"
+    Remove-item parcer.c
+    Remove-item parcer.h
+    Remove-item validator.exe
+    Remove-item lex.yy.c
+}
+if ($mode -eq "all" -or $mode -eq "build")
+{
+    echo "Build"
+    .\win_bison.exe -o parcer.c -d analyser.y 
+    .\win_flex.exe -o lexer.c lexer.l
+    gcc lexer.c parcer.c parcer.h -o validator}
+if ($mode -eq "all" -or $mode -eq "run")
+{
+    echo "Run"
+    Get-Content config.in | .\validator.exe
+}
